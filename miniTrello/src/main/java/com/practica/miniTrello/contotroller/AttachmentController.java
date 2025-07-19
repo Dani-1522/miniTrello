eaf1ad5ae0c297980a764044ebf2ec8811c9c9f5
Daniel Martinez <dm1653234@gmail.com>
2025-07-19 00:55:44 -0500

    update

--- miniTrello/src/main/java/com/practica/miniTrello/contotroller/AttachmentController.java
+++ miniTrello/src/main/java/com/practica/miniTrello/contotroller/AttachmentController.java
@@ -1,50 +1,47 @@
 package com.practica.miniTrello.contotroller;
 
-import com.practica.miniTrello.DTOs.AttachmentDTO;
-import com.practica.miniTrello.entity.Attachment;
+
+import com.practica.miniTrello.entity.CardAttachment;
+import com.practica.miniTrello.entity.User;
 import com.practica.miniTrello.service.AttachmentService;
-import org.springframework.core.io.Resource;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
 
+
 @RestController
-@RequestMapping("/api/attachments")
+@RequestMapping("/api/cards/{cardId}/attachments")
 @RequiredArgsConstructor
 public class AttachmentController {
 
     private final AttachmentService attachmentService;
 
-    @PostMapping("/{cardId}")
-    public ResponseEntity<AttachmentDTO> upload(@PathVariable Long cardId,
-                                                @RequestParam("file") MultipartFile file,
-                                                Authentication auth) throws IOException {
-        return ResponseEntity.ok(attachmentService.uploadFile(cardId, file, auth.getName()));
+    @PostMapping
+    public ResponseEntity<?> uploadAttachment(@PathVariable Long cardId,
+                                              @RequestParam("file")MultipartFile file,
+                                              @AuthenticationPrincipal User user) throws IOException {
+        attachmentService.uploadAttachment(cardId, file, user.getUsername());
+        return ResponseEntity.ok("archivo subido");
     }
 
-    @GetMapping("/card/{cardId}")
-    public ResponseEntity<List<AttachmentDTO>> list(@PathVariable Long cardId) {
-        return ResponseEntity.ok(attachmentService.getCardAttachments(cardId));
-    }
-
-    @GetMapping("/{attachmentId}/download")
-    public ResponseEntity<Resource> download(@PathVariable Long attachmentId) throws IOException {
-        Resource file = attachmentService.downloadFile(attachmentId);
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
-                .body(file);
+    @GetMapping
+    public ResponseEntity<List<CardAttachment>> getAttachments(@PathVariable Long cardId,
+                                                               @AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(attachmentService.getAttachments(cardId, user.getUsername()));
     }
 
     @DeleteMapping("/{attachmentId}")
-    public ResponseEntity<Void> delete(@PathVariable Long attachmentId,
-                                       Authentication auth) throws IOException {
-        attachmentService.deleteAttachment(attachmentId, auth.getName());
+    public ResponseEntity<?> deleteAttachment(@PathVariable Long cardId,
+                                              @PathVariable Long attachmentId,
+                                              @AuthenticationPrincipal User user) {
+        attachmentService.deleteAttachment(attachmentId, user.getUsername());
         return ResponseEntity.noContent().build();
+
     }
 }
--- miniTrello/src/main/java/com/practica/miniTrello/service/AttachmentService.java
+++ miniTrello/src/main/java/com/practica/miniTrello/service/AttachmentService.java
@@ -1,100 +1,94 @@
 package com.practica.miniTrello.service;
 
-import com.practica.miniTrello.DTOs.AttachmentDTO;
-import com.practica.miniTrello.entity.Attachment;
-import com.practica.miniTrello.entity.Card;
-import com.practica.miniTrello.entity.User;
-import com.practica.miniTrello.repository.AttachmentRepository;
-import com.practica.miniTrello.repository.CardRepository;
-import com.practica.miniTrello.repository.UserRepository;
 
+import com.practica.miniTrello.entity.Card;
+import com.practica.miniTrello.entity.CardAttachment;
+import com.practica.miniTrello.repository.CardActivityRepository;
+import com.practica.miniTrello.repository.CardAttachmentRepository;
+import com.practica.miniTrello.repository.CardRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.UrlResource;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.UUID;
-
 
 @Service
 @RequiredArgsConstructor
 public class AttachmentService {
 
-    @Value("${upload.dir}")
-    private String uploadDir;
+    private final CardActivityRepository cardActivityRepository;
+    private final CardRepository cardRepository;
 
-    private final AttachmentRepository attachmentRepo;
-    private final CardRepository cardRepo;
-    private final UserRepository userRepo;
+    private final Path rootLocation = Paths.get("storage");
+    private final CardAttachmentRepository cardAttachmentRepository;
 
-    public AttachmentDTO uploadFile(Long cardId, MultipartFile file, String username) throws IOException {
-        Card card = cardRepo.findById(cardId)
-                .orElseThrow(() -> new NoSuchElementException("Card not found"));
+    @Transactional
+    public void uploadAttachment(Long cardId, MultipartFile file, String username) throws IOException {
+        Card card = getVerifiedCard(cardId, username);
 
-        User user = userRepo.findByUsername(username)
-                .orElseThrow(() -> new NoSuchElementException("User not found"));
+        if (!Files.exists(rootLocation)) Files.createDirectories(rootLocation);
 
-        String filename = UUID.randomUUID() + "_" + file.getOriginalFilename();
-        Path path = Paths.get(uploadDir, filename);
-        Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+        String folder = "card-" + card.getId();
+        Path cardFolder = rootLocation.resolve(folder);
+        if (!Files.exists(cardFolder)) Files.createDirectories(cardFolder);
 
-        Attachment saved = attachmentRepo.save(Attachment.builder()
-                .filename(file.getOriginalFilename())
-                .fileType(file.getContentType())
-                .filePath(path.toString())
-                .uploadedAt(LocalDateTime.now())
-                .uploader(user)
+        String filename = file.getOriginalFilename();
+        Path destinantio = cardFolder.resolve(filename);
+
+        Files.copy(file.getInputStream(), destinantio, StandardCopyOption.REPLACE_EXISTING);
+
+        CardAttachment attachment = CardAttachment.builder()
                 .card(card)
-                .build());
+                .fileName(filename)
+                .filepath(destinantio.toString())
+                .contentType(file.getContentType())
+                .uploadedAt(LocalDateTime.now())
+                .build();
 
-        return toDTO(saved);
+        cardAttachmentRepository.save(attachment);
     }
 
+   public List<CardAttachment> getAttachments(Long cardId, String username) {
+        getVerifiedCard(cardId, username);
+        return cardAttachmentRepository.findByCardId(cardId);
+   }
 
-    public Resource downloadFile(Long attachmentId) throws IOException {
-        Attachment attachment = attachmentRepo.findById(attachmentId)
-                .orElseThrow(() -> new NoSuchElementException("Attachment not found"));
+   public void deleteAttachment(Long attachmentId, String username) {
+        CardAttachment attachment = cardAttachmentRepository.findById(attachmentId)
+                .orElseThrow(()-> new NoSuchElementException("Attachement: " + attachmentId + " not found"));
 
-        Path path = Paths.get(attachment.getFilePath());
-        return new UrlResource(path.toUri());
-    }
-
-    public List<AttachmentDTO> getCardAttachments(Long cardId) {
-        return attachmentRepo.findByCardId(cardId)
-                .stream()
-                .map(this::toDTO)
-                .toList();
-    }
-
-
-    public void deleteAttachment(Long attachmentId, String username) throws IOException {
-        Attachment attachment = attachmentRepo.findById(attachmentId)
-                .orElseThrow(() -> new NoSuchElementException("Attachment not found"));
-
-        if (!attachment.getUploader().getUsername().equals(username)) {
-            throw new AccessDeniedException("You can only delete your own attachments.");
+        if(!attachment.getCard().getBoardList().getBoard().getOwner().getUsername().equals(username)){
+            throw new AccessDeniedException("You don't have permission to delete this attachment");
         }
+        try {
+            Files.deleteIfExists(Paths.get(attachment.getFileName()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        cardAttachmentRepository.delete(attachment);
+   }
 
-        Files.deleteIfExists(Paths.get(attachment.getFilePath()));
-        attachmentRepo.delete(attachment);
+    public Card getVerifiedCard(Long cardId, String username) {
+        Card card = cardRepository.findById(cardId)
+                .orElseThrow(()-> new NoSuchElementException("Card not found"));
+
+        if (!card.getBoardList().getBoard().getOwner().getUsername().equals(username)) {
+            throw new AccessDeniedException("You don't have permission to access this resource");
+        }
+        return card;
     }
 
-    private AttachmentDTO toDTO(Attachment attachment) {
-        return new AttachmentDTO(
-                attachment.getId(),
-                attachment.getFilename(),
-                attachment.getFileType(),
-                attachment.getUploader().getUsername(),
-                attachment.getUploadedAt()
-        );
-    }
+
 
 }
